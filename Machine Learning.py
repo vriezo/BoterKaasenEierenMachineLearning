52690ae11fa606552a2c7c096ed5c267ef69cce0
vriezo <45482682+vriezo@users.noreply.github.com>
2021-01-04 14:27:12 +0100

    Update Machine Learning.py

--- Machine Learning.py
+++ Machine Learning.py
@@ -16,7 +16,9 @@ class MyAgent(MLAgent):
     
 random.seed(1)
  
-my_agent = MyAgent(alpha=0.8, epsilon=0.2)
+my_agent = MyAgent(alpha=0.8, epsilon=0.2) 
+#Dit zijn de hyperparameters alpha en epsilon. Alpha bepaalt hoe snel de agent iets leert. In dit geval is alpha redelijk hoog en zal dus snel nieuwe kennis als het ware opnemen. 
+#De epsilon bepaalt daarentegen hoe vaak de agent iets nieuws probeert in plaats van het gebruiken van al bekende informatie. Deze factor is daarom maar 0,2. Als deze factor hoger zou zijn zou de agent radom dingen proberen in plaats van het gebruiken van de best bekende zet.
 random_agent = RandomAgent()
  
 train_and_plot(
